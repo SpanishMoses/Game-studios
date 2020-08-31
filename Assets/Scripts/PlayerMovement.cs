5f521f7385d3d2cf788152f185050602307f794b
Sillverstriker525 <60528061+Sillverstriker525@users.noreply.github.com>
2020-08-30 19:04:34 -0500

    Dash Jump and skybox

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,15 +8,27 @@ public class PlayerMovement : MonoBehaviour
 
     public static PlayerMovement instance;
 
+    private Rigidbody rb;
+
+    public float defaultSpeed = 12f;
     public float speed = 12f;
     public float gravity = -9.81f;
 
     public Transform groundCheck;
     public float groundDistance = 0.4f; 
     public LayerMask groundMask;
+    public float jumpHight = 3f;
+
+    [SerializeField] private float dashForce = 50f;
+    [SerializeField] private float dashDuration = 0.2f;
 
     Vector3 velocity;
     bool isGrounded;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -35,8 +47,36 @@ public class PlayerMovement : MonoBehaviour
 
         controller.Move(move * speed * Time.deltaTime);
 
+        //Jump
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHight * -2f * gravity);
+        }
+
         velocity.y += gravity * Time.deltaTime;
 
         controller.Move(velocity * Time.deltaTime);
+
+        //Dash
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            StartCoroutine(Dash());
+        }
+    }
+
+    public IEnumerator Dash()
+    {
+        //rb.AddForce(Camera.main.transform.forward * dashForce, ForceMode.VelocityChange);
+
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+
+        speed = dashForce;
+
+        yield return new WaitForSeconds(dashDuration);
+
+        speed = defaultSpeed;
+
+        rb.velocity = Vector3.zero;
     }
 }
