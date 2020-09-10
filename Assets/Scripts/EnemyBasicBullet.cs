111e65f0d90bf9e35653e5d9ae051491b2e1fd1e
SpanishMoses <50852159+SpanishMoses@users.noreply.github.com>
2020-09-10 19:51:04 -0400

    progress with shooting
    enemy fires bullet but needs extreme fixing

---
+++ Assets/Scripts/EnemyBasicBullet.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBasicBullet : MonoBehaviour
+{
+    //https://www.youtube.com/watch?v=_Z1t7MNk0c4&t=682s
+
+    public float speed;
+
+    private Transform player;
+    private Vector2 target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        target = new Vector2(player.position.x, player.position.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
+            if (transform.position.x == target.x && transform.position.y == target.y)
+            {
+                DestroyProjectile();
+
+                Destroy(gameObject, 9f);
+            }
+    }
+
+    void DestroyProjectile()
+    {
+        Destroy(gameObject);
+    }
+ }
---
+++ Assets/Scripts/EnemyShoot.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShoot : MonoBehaviour
+{
+    public GameObject bullet;
+    EnemyMove mov;
+    public GameObject parent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mov = GetComponent<EnemyMove>();
+        StartCoroutine(shoot());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if (mov.locActive == true){
+            
+        }*/
+    }
+
+    IEnumerator shoot(){
+        yield return new WaitForSeconds(6);
+        Instantiate(bullet, parent.transform.position, Quaternion.identity);
+        StartCoroutine(shoot());
+    }
+}
