f6fee960460ffe99db43b64acdfa06e7acfce6ac
SpanishMoses <50852159+SpanishMoses@users.noreply.github.com>
2020-09-11 23:48:20 -0400

    Hit scan
    Hit scan works

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
+    //hit scan help from https://www.youtube.com/watch?v=THnivyG0Mvo&ab_channel=Brackeys
 
     public float mouseSensitivity = 100f;
 
@@ -11,10 +12,15 @@ public class MouseLook : MonoBehaviour
 
     float xRotation = 0f;
 
+    public float dist = 100f;
+
+    public bool shootReady;
+
     // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        shootReady = true;
     }
 
     // Update is called once per frame
@@ -28,5 +34,24 @@ public class MouseLook : MonoBehaviour
 
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
+
+        if (Input.GetKey(KeyCode.Mouse0) && shootReady == true){
+            Shoot();
+            StartCoroutine(reload());
+        }
+    }
+
+    void Shoot(){
+        RaycastHit hit;
+        if (Physics.Raycast(playerBody.position, playerBody.transform.forward, out hit, dist))
+        {
+            Debug.Log(hit.transform.name);
+        }
+    }
+
+    IEnumerator reload(){
+        shootReady = false;
+        yield return new WaitForSeconds(2);
+        shootReady = true;
     }
 }
