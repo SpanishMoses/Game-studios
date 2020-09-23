64e5144b2d83344438df860755665c0d962da9c3
SpanishMoses <50852159+SpanishMoses@users.noreply.github.com>
2020-09-23 15:38:57 -0400

    pixie animations

--- Assets/Scripts/TextManager.cs
+++ Assets/Scripts/TextManager.cs
@@ -8,21 +8,29 @@ public class TextManager : MonoBehaviour
 
     //code from https://www.youtube.com/watch?v=f-oSXg6_AMQ&t=336s
 
+    public Animator anim;
+    public GameObject pixie;
+    public GameObject textBox;
+    public GameObject reticle;
+    public float speed = 0.5f;
+
     public Text text;
     public string[] sentences;
     public int index;
     public float typingSpeed;
     public int sentenceNum;
 
-    public Animator anim;
-
     private PlayerMovement player;
+
+    RectTransform rect;
 
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(Type());
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>();
+        anim = pixie.gameObject.GetComponent<Animator>();
+        rect = pixie.gameObject.GetComponent<RectTransform>();
     }
 
     // Update is called once per frame
@@ -30,13 +38,28 @@ public class TextManager : MonoBehaviour
     {
         if (text.text == sentences[index] && sentenceNum < 9){
             player.cantMove = false;
+            anim.SetFloat("Blend", 0);
+            
         } else if (sentenceNum == 9){
             player.cantMove = true;
             if (text.text == sentences[index] && sentenceNum == 9 && Input.GetKey(KeyCode.Mouse0)){
                 player.cantMove = false;
+                anim.SetBool("MousClick", true);
+                StartCoroutine(whipe());
                 sentenceNum++;
             }
+
+            if (sentenceNum == 9){
+                anim.SetBool("TutDone", true);
+                
+            }
         }
+    }
+
+    IEnumerator whipe(){
+        yield return new WaitForSeconds(3f);
+        pixie.SetActive(false);
+        textBox.SetActive(false);
     }
 
     public IEnumerator Type()
@@ -44,7 +67,7 @@ public class TextManager : MonoBehaviour
         foreach (char letter in sentences[index].ToCharArray())
         {
             text.text += letter;
-            //anim.SetFloat("Blend", 1);
+            anim.SetFloat("Blend", 1);
             yield return new WaitForSeconds(typingSpeed);
         }
     }
