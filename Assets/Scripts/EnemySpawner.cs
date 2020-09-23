7ee595688aa10845f1025ed065b306f16ae8fff8
SpanishMoses <50852159+SpanishMoses@users.noreply.github.com>
2020-09-22 22:01:11 -0400

    enemy spawner

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    public GameObject[] enemies;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < enemies.Length; i++){
+            enemies[i].SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player"){
+            for (int i = 0; i < enemies.Length; i++)
+            {
+                enemies[i].SetActive(true);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
