7396faa80c2bb3dccd0e1fa780e59e305cd125da
SpanishMoses <50852159+SpanishMoses@users.noreply.github.com>
2020-09-28 04:25:06 -0400

    Charging enemy DONE!!!!!

--- Assets/Scripts/enemy scripts/ChargingEnemy.cs
+++ Assets/Scripts/enemy scripts/ChargingEnemy.cs
@@ -9,91 +9,74 @@ public class ChargingEnemy : MonoBehaviour
     private Vector3 normalizeDirection;
 
     public float playerDist;
-    public float backUpDist;
     public float speed;
 
     private Transform playerLoc;
 
-    NavMeshAgent navMeshAgent;
+    public float time;
+    public float timeBetweenCharges;
 
     public bool locActive;
-    public bool backUp;
     public bool isCharging;
 
     // Start is called before the first frame update
     void Start()
     {
         playerLoc = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-        navMeshAgent = this.GetComponent<NavMeshAgent>();
+        isCharging = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Vector2.Distance(transform.position, playerLoc.transform.position) < playerDist && Vector2.Distance(transform.position, playerLoc.transform.position) > backUpDist && isCharging == false)
-        {
-            locActive = true;
-            backUp = false;
-        }
-        if (Vector2.Distance(transform.position, playerLoc.transform.position) < backUpDist && isCharging == false)
-        {
-            backUp = true;
-            locActive = false;
-        }
-
-        if (backUp == false)
+        if (Vector2.Distance(transform.position, playerLoc.transform.position) < playerDist && isCharging == false)
         {
             locActive = true;
         }
 
-        if (locActive == true && backUp == false && isCharging == false)
+        if (locActive == true && isCharging == false)
         {
-            navMeshAgent = this.GetComponent<NavMeshAgent>();
-            SetDestination();
-        }
-        if (backUp == true && locActive == false && isCharging == false)
-        {
-            AwayDestination();
+            time += Time.deltaTime;
+            if (time >= timeBetweenCharges)
+            {
+                charge();
+                time = 0;
+                locActive = false;
+                speed = 20;
+                
+                
+            }
         }
 
         if (isCharging == true){
-            navMeshAgent.enabled = false;
-            speed = 10;
-            
-            transform.position += normalizeDirection* speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+            if (transform.position.x == target.x && transform.position.y == target.y && transform.position.z == target.z)
+            {
+                isCharging = false;
+                speed = 0;
+                Debug.Log("made it");
+                locActive = true;
+            }
         }
     }
 
-    void SetDestination()
-    {
-        if (playerLoc.transform.position != null)
-        {
-            Vector3 targetVector = playerLoc.transform.position;
-            navMeshAgent.SetDestination(targetVector);
-        }
-    }
-
-    void AwayDestination()
-    {
-        float distance = Vector3.Distance(transform.position, playerLoc.transform.position);
-
-        if (distance <= backUpDist)
-        {
-            Vector3 dirToPlayer = transform.position - playerLoc.transform.position;
-            Vector3 newPos = transform.position + dirToPlayer * 2;
-            navMeshAgent.SetDestination(newPos);
-            StartCoroutine(charge());
-        }
-    }
-
-    IEnumerator charge(){
+    void charge(){
         target = new Vector3(playerLoc.position.x, playerLoc.position.y, playerLoc.position.z);
-        normalizeDirection = (target - transform.position).normalized;
-        yield return new WaitForSeconds(2f);
         isCharging = true;
-        yield return new WaitForSeconds(2f);
-        isCharging = false;
-        navMeshAgent.enabled = true;
+
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player" && isCharging == true)
+            {
+                PlayerMovement playerMovement = collision.transform.GetComponent<PlayerMovement>();
+                if (playerMovement != null)
+                {
+                    playerMovement.TakeDamage(1f);
+                }
+            }
     }
 }
 
