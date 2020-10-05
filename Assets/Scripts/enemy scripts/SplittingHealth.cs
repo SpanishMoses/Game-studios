8ddc283a233ed6458f453598484fb1d847785f41
SpanishMoses <50852159+SpanishMoses@users.noreply.github.com>
2020-10-05 01:00:05 -0400

    splitting enemy done

---
+++ Assets/Scripts/enemy scripts/SplittingHealth.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplittingHealth : MonoBehaviour
+{
+    //enemy spawning cdoe from Dan from food friendz
+
+    public GameObject parent;
+    public Transform leftPt;
+    public Transform rightPt;
+
+
+    public GameObject littleBois;
+
+    public float time;
+    public float maxTimer;
+
+    public EnemyHealth health;
+
+    private void Update()
+    {
+        if (health.health <= health.minHealth)
+        {
+            time += Time.deltaTime;
+            if (time >= maxTimer){
+                Instantiate(littleBois, leftPt.position, Quaternion.identity);
+                Instantiate(littleBois, rightPt.position, Quaternion.identity);
+                time = 0;
+                
+            }
+            
+            
+        }
+    }
+
+    void spawn(){
+        
+    }
+
+    
+    
+}
