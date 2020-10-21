1eb273699ed63cdad0d4015520f05a7210d2472a
SpanishMoses <50852159+SpanishMoses@users.noreply.github.com>
2020-10-20 23:21:23 -0400

    did some stuff but got too tired

---
+++ Assets/Scripts/enemy scripts/BossDrone.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossDrone : MonoBehaviour
+{
+
+    public GameObject Boss;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(Boss.transform.position, Vector3.up, 20 * Time.deltaTime);
+    }
+}
