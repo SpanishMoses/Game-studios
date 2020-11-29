ff56dfc1376be3c6403a00fb3920dc1639dec727
Silverstriker525 <60528061+Sillverstriker525@users.noreply.github.com>
2020-11-29 12:53:05 -0500

    Added Weapon Switch animation

---
+++ Assets/Scripts/WeaponSwitch.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSwitch : MonoBehaviour
+{
+
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Alpha3) || Input.GetKeyDown(KeyCode.Alpha4) || Input.GetKeyDown(KeyCode.Alpha5) || Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            anim.SetTrigger("WeaponSwitch");
+        }
+    }
+}
