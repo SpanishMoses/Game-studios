d82be0e42fe93e2ba011f13b5c6e3e8e0f9e89b9
SpanishMoses <50852159+SpanishMoses@users.noreply.github.com>
2021-09-24 22:01:50 -0400

    all changes made

---
+++ Assets/Scripts/powerfade.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class powerfade : MonoBehaviour
+{
+
+    public float time;
+    public float maxTime;
+
+    public SpriteRenderer sprite;
+
+    public float spriteFade;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+
+        if (time > 15){
+            sprite.color -= new Color(0, 0, 0, spriteFade * Time.deltaTime);
+        }
+
+        if (time >= maxTime){
+            Destroy(gameObject);
+        }
+    }
+}
